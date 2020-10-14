3b2ac85a9bbd144308956dc6dec6032b3c3b5456
SungCheol Jang <scjang.dev@gmail.com>
2020-10-14 15:48:59 +0900

    feat: Add api test page
    - First of all GET method

---
+++ src/pages/api_test/get/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '~components/Layout';
+
+const get = () => {
+  return (
+    <Layout title="Api Test GET | Next.js + TypeScript Example">
+      <p>GET</p>
+    </Layout>
+  )
+}
+
+export default get
