95639663856d11053e70c340609fe52420367105
SungCheol Jang <scjang.dev@gmail.com>
2020-10-20 00:24:00 +0900

    Refactor: Refactoring for no api parameters(#7)

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,25 +2,14 @@ import { Request, Response, ErrorRequestHandler, NextFunction } from 'express'
 import axios from 'axios'
 import apiMap from './map'
 
-interface ApiParam {
-  key: string
-  data: object
-}
-
-/* interface TestParam {
-  url?: string
-  params?: Array<string>
-  data?: {[index: string]: any}
-  body?: object
-}*/
-
 const _findParamsInUrl = (url: string) => {
   const regexp = /(?::)(\D\w+)/gim
   const params = url.match(regexp)
   return params && params.length > 0 ? params.map((match) => match.replace(':', '')) : []
 }
 
-const _makeRequestUrl = (url: string, params: Array<string>, data: {[index: string]: any}) => {
+
+const _makeRequestUrl = (url: string, params: Array<string>, data: { [key: string]: any }) => {
   if (!params) return url
   let requestUrl = url
   params.forEach((param) => {
@@ -30,7 +19,7 @@ const _makeRequestUrl = (url: string, params: Array<string>, data: {[index: stri
 }
 
 const _bodyBuilder = (data: object, params: Array<string>, body: object) => {
-  const _body: {[index: string]: any} = Object.assign({}, data, body)
+  const _body: { [index: string]: any } = Object.assign({}, data, body)
   if (!params) return _body
   params.forEach((param) => {
     if (_body[param]) delete _body[param]
@@ -38,14 +27,14 @@ const _bodyBuilder = (data: object, params: Array<string>, body: object) => {
   return _body
 }
 
-const _getApiOptions = (key: string, data: object) => {
+const _getApiOptions = (config: { [key: string]: any }) => {
   const headers = {
     'Authorization': 'Basic ' + Buffer.from('seoulstore:devteam!').toString('base64'),
   }
-  const apiOptions: any = apiMap(key)
+  const apiOptions: any = apiMap(config.key)
   const params = _findParamsInUrl(apiOptions.url)
-  const requestUrl = _makeRequestUrl(apiOptions.url, params, data)
-  const body = _bodyBuilder(apiOptions.data, params, data)
+  const requestUrl = _makeRequestUrl(apiOptions.url, params, config.data)
+  const body = _bodyBuilder(apiOptions.data, params, config.data)
 
   return {
     url: requestUrl,
@@ -56,17 +45,9 @@ const _getApiOptions = (key: string, data: object) => {
   }
 }
 
-// Use for useSwr
-// because don't need proxy at server side
-export const proxy = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req)
-  console.log(res)
-  console.log(next)
-}
-
 // Use for next serverside app
-export const api = async ({ key, data }: ApiParam, req?: Request) => {
-  const options = _getApiOptions(key, data)
+export const api = async (config: { [key: string]: any }, req?: Request) => {
+  const options = _getApiOptions(config)
   const next = (err: ErrorRequestHandler) => {
     console.log(`Api error from ${req ? 'server' : 'front'}`, err)
     throw err
@@ -75,4 +56,11 @@ export const api = async ({ key, data }: ApiParam, req?: Request) => {
   return axios(options)
       .then((res) => res.data)
       .catch(next)
+}
+
+// because don't need proxy at server side
+export const proxy = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req)
+  console.log(res)
+  console.log(next)
 }
