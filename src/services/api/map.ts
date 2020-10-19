f3c4a825349c095685095d8795b36c1b2f2f4fdb
SungCheol Jang <scjang.dev@gmail.com>
2020-10-20 00:17:13 +0900

    refactor: Add a api map(#7)
    - getCategories

--- src/services/api/map.ts
+++ src/services/api/map.ts
@@ -1,8 +1,12 @@
 export default (key: string) => {
-  const map: {[index: string]: object} = {
-    'getProduct': {
-      'url': 'https://gateway-stage.seoulstore.com/siteProducts/:id',
-      'method': 'GET',
+  const map: { [index: string]: object } = {
+    getProduct: {
+      url: 'https://gateway-stage.seoulstore.com/siteProducts/:id',
+      method: 'GET',
+    },
+    getCategories: {
+      url: 'https://gateway-stage.seoulstore.com/siteCategories',
+      method: 'GET',
     },
   }
   return map[key]
