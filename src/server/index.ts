8e98d9df662339d826a73ee00e09471e20a2d6b5
byoungjun kwon <finalsoul1@gmail.com>
2020-10-26 16:24:59 +0900

    Add prettier Add next.config.js Fixes .eslintrc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,32 +1,31 @@
-import express, { Request, Response, Router } from 'express'
-import next from 'next'
+import express, { Router, Request, Response } from 'express';
+import next from 'next';
+import { proxy } from '~services/api';
 
-import { proxy } from '~services/api'
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+const router = Router();
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-const port = process.env.PORT || 3000
-const router = Router()
-
-router.post('/api/:name', proxy)
+router.post('/api/:name', proxy);
 
 router.get('*', (req: Request, res: Response) => {
-  return handle(req, res)
+	return handle(req, res);
 });
 
 (async () => {
-  try {
-    await app.prepare()
-    const server = express()
-    server.use(router)
+	try {
+		await app.prepare();
+		const server = express();
+		server.use(router);
 
-    server.listen(port, (err?: any) => {
-      if (err) throw err
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
-    })
-  } catch (e) {
-    console.error(e)
-    process.exit(1)
-  }
-})()
+		server.listen(port, (err?: any) => {
+			if (err) throw err;
+			console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+		});
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+})();
--- src/services/api/map.ts
+++ src/services/api/map.ts
@@ -1,13 +1,13 @@
 export default (key: string) => {
-  const map: { [index: string]: object } = {
-    getProduct: {
-      url: 'https://gateway-stage.seoulstore.com/siteProducts/:id',
-      method: 'GET',
-    },
-    getCategories: {
-      url: 'https://gateway-stage.seoulstore.com/siteCategories',
-      method: 'GET',
-    },
-  }
-  return map[key]
-}
+	const map: { [index: string]: { url: string; method: string } } = {
+		getProduct: {
+			url: 'https://gateway-stage.seoulstore.com/siteProducts/:id',
+			method: 'GET',
+		},
+		getCategories: {
+			url: 'https://gateway-stage.seoulstore.com/siteCategories',
+			method: 'GET',
+		},
+	};
+	return map[key];
+};
