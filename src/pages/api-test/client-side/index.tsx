b324e4eee5021a1e421ba72e3d95eef8e2d35584
SungCheol Jang <scjang.dev@gmail.com>
2020-10-30 02:22:48 +0900

    refactor: Add api test page (#7)

---
+++ src/pages/api-test/client-side/index.tsx
@@ -0,0 +1,29 @@
+// todo: should fix error
+// this page has gateway-stage connection error
+
+import React, { useEffect } from 'react'
+import Layout from '~components/Layout'
+import { api } from '~services/api'
+
+const clientSide = ({ name }: any) => {
+  useEffect(() => {
+    const getProductData = async () => {
+      await api({
+        key: 'getProduct',
+        data: {
+          id: 1278016,
+        },
+      })
+    }
+    console.log('in client-side', getProductData())
+  }, [])
+  return (
+    <Layout title="Api Test on ClientSide">
+      <p>
+        Get product name is <span style={{ fontWeight: 'bold', color: 'blue' }}>{name}</span>
+      </p>
+    </Layout>
+  )
+}
+
+export default clientSide
