a18b2f12691ccf7f4d747f8e97b4871f55d423c9
SungCheol Jang <scjang.dev@gmail.com>
2020-10-30 01:17:09 +0900

    refactor: Refactoring application start (#7)
    - Split express setting in server/index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
-/*
-import express, { Router, Request, Response } from 'express'
+import { Router, Request, Response } from 'express'
 import next from 'next'
+import express from './express'
 import { proxy } from '~services/api'
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -16,55 +16,10 @@ router.get('*', (req: Request, res: Response) => {
 })
 
 app.prepare().then(() => {
-  const server = express()
-  server.use(router)
-  server.use(cors)
+  const server = express(router)
 
   server.listen(port, (err?: any) => {
     if (err) throw err
     console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
   })
-})
-
-/!*;(async () => {
-  try {
-    await app.prepare()
-    const server = express()
-    server.use(router)
-
-    server.listen(port, (err?: any) => {
-      if (err) throw err
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
-    })
-  } catch (e) {
-    console.error(e)
-    process.exit(1)
-  }
-})()*!/
-*/
-
-import { Router, Request, Response } from 'express'
-import next from 'next'
-import express from './express'
-import { proxy } from '~services/api'
-
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-const port = process.env.PORT || 3000
-const router = Router()
-
-router.post('/api/:name', proxy)
-
-router.get('*', (req: Request, res: Response) => {
-	return handle(req, res)
-})
-
-app.prepare().then(() => {
-	const server = express(router)
-	
-	server.listen(port, (err?: any) => {
-		if (err) throw err
-		console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
-	})
 })
