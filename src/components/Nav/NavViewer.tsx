9a77c06a457f80950f2b18a81523dc9c06bf3276
byoungjun kwon <finalsoul1@gmail.com>
2020-11-05 02:02:51 +0900

    Remove comments

--- src/components/Nav/NavViewer.tsx
+++ src/components/Nav/NavViewer.tsx
@@ -1,48 +1,7 @@
 import React from 'react'
 import { Heading, Flex } from 'rebass'
 import styled from '@emotion/styled'
-// import uniqId from 'uniqid'
 
-// const navItems = [
-//   {
-//     id: uniqId('nav_'),
-//     name: '홈',
-//     href: '/',
-//     prod: true,
-//   },
-//   {
-//     id: uniqId('nav_'),
-//     name: '트랙보기',
-//     href: '/tracks',
-//     prod: true,
-//   },
-//   {
-//     id: uniqId('nav_'),
-//     name: '구매한 음악',
-//     href: '/me/purchases',
-//     requiredAuth: true,
-//     prod: true,
-//   },
-//   {
-//     id: uniqId('nav_'),
-//     name: '라이선스',
-//     href: '/me/licenses?filter=all',
-//     requiredAuth: true,
-//     prod: true,
-//   },
-//   {
-//     id: uniqId('nav_'),
-//     name: '이용방법',
-//     href: '/how-it-works',
-//     prod: true,
-//   },
-//   {
-//     id: uniqId('nav_'),
-//     name: '블로그',
-//     href: '/blog',
-//     prod: true,
-//   },
-// ]
 const NavBox = styled(Flex)`
   width: 18%;
   min-width: 300px;
