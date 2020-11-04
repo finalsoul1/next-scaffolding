bdd6bef63050962247d7536869070180390882fa
byoungjun kwon <finalsoul1@gmail.com>
2020-11-05 01:52:07 +0900

    Add theme & reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import type { AppProps } from 'next/app'
 import { NextPageContext, NextComponentType } from 'next'
 import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
+import { Reset } from 'styled-reset'
 import theme from '~theme/index'
+import MainTemplate from '~components/Template/Main'
 
 interface ForGetInitialProps {
   Component: NextComponentType
@@ -10,10 +12,14 @@ interface ForGetInitialProps {
 }
 
 const RootApp = ({ Component, pageProps }: AppProps) => {
-  return
-  <ThemeProvider theme={theme}>
-  <Component {...pageProps.props} />
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <MainTemplate>
+        <Reset />
+        <Component {...pageProps.props} />
+      </MainTemplate>
+    </ThemeProvider>
+  )
 }
 
 RootApp.getInitialProps = async ({ Component, ctx }: ForGetInitialProps) => {
